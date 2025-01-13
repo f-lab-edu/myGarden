3e97d2e55b2d542cc829feda513aaae846836daf
ej <97089140+zeze95@users.noreply.github.com>
2025-01-14 05:43:37 +0900

    [fix] home tab mvvmc 패턴으로 수정

--- myGarden/Feature/Home/HomeViewModel.swift
+++ myGarden/Feature/Home/HomeViewModel.swift
@@ -7,14 +7,20 @@
 
 import UIKit
 
-class HomeViewModel {
-    
-    
+
+final class HomeViewModel {
+    var coordinator : HomeCoordinator
+    init(coordinator: HomeCoordinator) {
+        self.coordinator = coordinator
+    }
+
     func reset() { // 온보딩 테스트용으로 생성
         print("reset")
         UserDefaults.standard.removeObject(forKey: "isFirstTime")
         UserDefaults.standard.synchronize()
         print("\(UserDefaults.standard.removeObject(forKey: "isFirstTime"))")
     }
-    
+    final func diaryAddClicked() {
+        coordinator.diaryAdd()
+    }
 }
